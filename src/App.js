abcd1e44db99b6611bb2d12b8d808f66d892608d
adsznzhang <adszn@hotmail.com>
2017-07-29 21:43:10 +0800

    setup Marked and Highlight

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import marked,{Renderer} from 'marked';
+import highlight from 'highlight.js'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {content: '### Type Markdown Here!'}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    this.setState ( {content: event.target.value} )
+  }
+
+  rawMarkup() {
+	  marked.setOptions({
+		  renderer: new marked.Renderer(),
+		  gfm: true,
+		  tables: true,
+		  breaks: false,
+		  pedantic: false,
+		  sanitize: true,
+		  smartLists: true,
+		  smartypants: false,
+		  highlight: function (code) {
+			  return require('highlight.js').highlightAuto(code).value
+		  }
+	  })
+
+	  var rawMarkup = marked(this.state.content, {sanitize: true})
+	  return {
+		  __html: rawMarkup
+	  }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container-fluid">
+        <div className="row">
+          <h1 className="text-center">ReactJS Markdown Editor</h1>
+          <div className="col-xs-12 col-sm-6">
+            <h3>Markdown</h3>
+            <textarea  id="markdown" className="markdown" defaultValue={this.state.content} onChange={this.handleChange}>
+            </textarea>
+          </div>
+          <div className="col-xs-12 col-sm-6">
+            <h3>Preview</h3>
+            <div id="preview" dangerouslySetInnerHTML={this.rawMarkup()} ></div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
